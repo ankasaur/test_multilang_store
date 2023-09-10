62f9cd3d5e33a550d59ea18369a2a5676c353a0b
Анна Барахтенко <a.barakhtenko@ispsystem.com>
2023-09-10 12:19:16 +0700

    added test files

---
+++ test_items.py
@@ -0,0 +1,7 @@
+gitfrom selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_add_to_basket_is_present(browser):
+    browser.get(link)
+    assert browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket'), "Add to basket button is not found"
